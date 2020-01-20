5726ffe48538684f8cb8cc0aa5c133640c736d21
nissy <35794374+nissy-insfc@users.noreply.github.com>
2020-01-20 16:24:24 +0900

    commit

---
+++ nodeex/weather-app/app.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const url = 'https://api.darksky.net/forecast/YOURTOKEN/37.8267,-122.4233'
+
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+})
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=YOURTOKEN&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
+})
