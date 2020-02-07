87d9c49e64055387ff470d45d3b5a7be3d89410d
nissy <s16660tn@sfc.keio.ac.jp>
2020-02-08 04:46:55 +0900

    add using middleware function

--- nodeex/myapp/app.js
+++ nodeex/myapp/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 
-//app.get('/',(req,res) => res.send('Hello world!'));
+
+var mylogger = function(req,res,next){
+	console.log('LOGGED');
+	next();
+}
+
+app.use(mylogger);
 
 app.get('/',function(req,res){
 	res.send('root');
 });
 
+/*
 app.get('/users/:userId/books/:bookId',function(req,res){
 	res.send('Your ID is '+req.params.userId+' and book ID is '+req.params.bookId);
 });
-
+*/
 
 app.listen(3000, () => console.log('Example app listening on the port 3000!'));
 
