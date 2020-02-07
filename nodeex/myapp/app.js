c120e3995e93a7f46b2624560e7d1d7997a173f6
nissy <s16660tn@sfc.keio.ac.jp>
2020-02-08 04:00:34 +0900

    add change

--- nodeex/myapp/app.js
+++ nodeex/myapp/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res) => res.send('Hello world!'));
+//app.get('/',(req,res) => res.send('Hello world!'));
+
+app.get('/',function(req,res){
+	res.send('GET request to the page');
+});
+
+app.post('/',function(req,res){
+	res.send('POST request to the page');
+});
 
 app.listen(3000, () => console.log('Example app listening on the port 3000!'));
+
+
+
