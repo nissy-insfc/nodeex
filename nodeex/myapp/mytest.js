3a948b186d44490425966589a2d135b40ee6bfba
nissy <s16660tn@sfc.keio.ac.jp>
2020-02-08 05:55:12 +0900

    fix 検索結果

--- nodeex/myapp/mytest.js
+++ nodeex/myapp/mytest.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var mw = require('./mw.js');
+const express = require('express');
+const  app = express();
+const mw = require('./mw.js');
 
 app.use(mw({option1:'1',option2:'2'}));
 
