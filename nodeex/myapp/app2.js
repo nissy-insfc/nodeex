e7d8b45231e8bf81706361d97330e1b8201ff1b9
nissy <s16660tn@sfc.keio.ac.jp>
2020-02-08 07:11:28 +0900

    add routing by id

--- nodeex/myapp/app2.js
+++ nodeex/myapp/app2.js
@@ -1,4 +1,34 @@
 const express = require('express');
 const app = express();
 
+app.get('/user/:id',function(req,res,next){
+//    if the user ID is 0, skip to the next route
+    if(Number(req.params.id)===0){
+        next('route');
+    }else{
+        next();
+    }
+},function(req,res,next){
+    // render a regular page
+    res.send('regular page');
+    }
+)
+
+// handler for the /user/:id path, which renders a special page
+app.get('/user/:id',function(req,res,next){
+    res.send('special page');
+})
+
+
+//root
+app.get('/',function(req,res){
+        var responseText = 'Hello World!<br>';
+        res.send(responseText);
+});
+
+
+
+
+app.listen(3000);
+
 
