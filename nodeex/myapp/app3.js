0eff2f48a2d9be409cbaeb1c510afa6186e18252
nissy <s16660tn@sfc.keio.ac.jp>
2020-02-09 04:02:13 +0900

    add router

---
+++ nodeex/myapp/app3.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+router.use(function(req,res,next){
+    console.log('Time:',Date.now());
+    next();
+});
+
+
+router.use('/user/:id',function(req,res,next){
+    console.log('Request URL:',req.originalUrl);
+    next();
+
+},function(req,res,next){
+    console.log('Request Type:',req.method);
+    next();
+});
+
+
+router.get('/user/:id',function(req,res,next){
+    if(Number(req.params.id)===0){
+        next('route');
+    }else{
+        next();
+    }
+},function(req,res,next){
+    res.send('regular');
+});
+
+router.get('/user/:id',function(req,res,next){
+    console.log(req.params.id);
+    res.send('special');
+});
+
+
+app.use('/',router);
+app.listen(3000);
