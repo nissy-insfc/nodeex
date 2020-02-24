5a1740df1cc14ac7d9dec175af2f31acf424e74f
nissy <s16660tn@sfc.keio.ac.jp>
2020-02-24 14:40:26 +0900

    add socket.io.js

--- nodeex/chat-app/app.js
+++ nodeex/chat-app/app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = 3001;
 
 app.get('/' , function(req, res){
    res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection',function(socket){
+        console.log('connected');
+});
+
 
 http.listen(PORT, function(){
       console.log('server listening. Port:' + PORT);
